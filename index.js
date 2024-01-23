234146ac4c2df531a80fea0f24a863a54efdcb17
farish-jamal <farishjamal98@gmail.com>
2024-01-23 23:28:48 +0530

    Status Codes

--- index.js
+++ index.js
@@ -23,21 +23,31 @@ app.get("/api/users", (req, res) => {
 app.route("/api/users/:id").get((req, res) => {
    const id = Number(req.params.id);
    const user = users.find((user) => user.id === id);
-   return res.json(user);
+   if(!user) return res.status(404).json({"Error": "No User Found"});
+   return res.status(200).json(user);
 }).patch((req, res) => {
    const id = Number(req.params.id);
    const user = users.find((user) => user.id === id);
+   if(!user) return res.status(404).json({"Error": "No User Found"});
    const body = req.body;
    user.first_name = body.first_name;
    user.job_profile = body.job_profile;
    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-      return res.send(`${id} updated Sucessfully`);
+      if(err){
+         return res.status(500);
+      }else{
+         return res.status(200).send(`${id} updated Sucessfully`);
+      }
    })
 }).delete((req, res) => {
    const id = Number(req.params.id);
    delete users[id - 1];
    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-      return res.send(`${id} deleted Sucessfully`);
+      if(err){
+         return res.status(500);
+      }else{
+         return res.status(200).send(`${id} deleted Sucessfully`);
+      }
    })
 })
 
@@ -45,7 +55,11 @@ app.post("/api/users", (req, res) => {
    const body = req.body;
    users.push({ ...body, id: users.length + 1 });
    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-      return res.json({ "status": "success", id: users.length });
+      if(err){
+         return res.status(500);
+      }else{
+         return res.status(201).json({ "status": "success", id: users.length });
+      }
    })
 })
 
