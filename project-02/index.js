d445221b4a19270ca9d4329d84c2959422863fe6
farish-jamal <farishjamal98@gmail.com>
2024-01-26 01:19:05 +0530

    Server Side Done

--- project-02/index.js
+++ project-02/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const { mongoDbConnection } = require("./database/db");
 const urlRoute = require("./routers/url.routers");
@@ -6,7 +7,11 @@ const urlRoute = require("./routers/url.routers");
 const app = express();
 const PORT = 8001;
 
+app.set('view engine', 'ejs');
+app.set('views', path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 //Database Connection
 mongoDbConnection(
--- project-02/routers/url.routers.js
+++ project-02/routers/url.routers.js
@@ -4,20 +4,30 @@ const Url = require("../models/url.models");
 
 const router = express.Router();
 
+
 router.post("/", async (req, res) => {
   const body = req.body;
+  console.log(body);
   const short = shortID.generate();
   await Url.create({
     shortId: short,
     redirectId: body.url,
   });
-  return res.status(201).json({ msg: "success", id: short });
+  return res.redirect("/api/url");
 });
+
+router.get("/", async(req, res)=>{
+  const urls = await Url.find({});
+  res.render("home",{
+    urls: urls,
+  });
+})
 
 router.get("/:shortId", async (req, res) => {
   const result = await Url.findOneAndUpdate(
     { shortId: req.params.shortId },
-    { $inc: { numberOfVisits: 1 } }
+    { $inc: { numberOfVisits: 1 } },
+    { new: true }
   );
   return res.redirect(result.redirectId);
 });
