8899ed687b5cb71293d86d0b8af7c9db76cac854
farish-jamal <farishjamal98@gmail.com>
2024-01-29 19:40:40 +0530

    response generation done

--- chatgpt/controllers/response.controller.js
+++ chatgpt/controllers/response.controller.js
@@ -2,9 +2,9 @@ const OpenAI =  require("openai");
 const openai = new OpenAI({
  apiKey: "",
 });
-async function main() {
+async function main(prompt) {
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "tell me a joke" }],
+    messages: [{ role: "system", content: prompt }],
     model: "gpt-3.5-turbo",
   });
   return completion.choices[0].message.content;
@@ -13,7 +13,7 @@ async function main() {
 
 async function handleOpenAiResponse(req, res){
  const {prompt} = req.body;
- const response = await main();
+ const response = await main(prompt);
  return res.render("home", {
   response: response,
   prompt: prompt 
